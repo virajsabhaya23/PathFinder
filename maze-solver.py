ede974d1cd7c0c258432b5d3ad9005f2cb734c93
Viraj <77448246+virajsabhaya23@users.noreply.github.com>
2022-03-28 06:27:57 -0500

    Fixed maze structure

--- maze-solver.py
+++ maze-solver.py
@@ -9,18 +9,31 @@ maze = [
     ["#"," ","#"," ","#","#","#","#","#","#"," ","#"],
     ["#"," ","#"," ","#","#","#","#"," ","#"," ","#"],
     ["#"," ","#"," ","#"," "," "," "," "," "," ","#"],
-    ["#"," ","#"," ","#"," ","#"," "," ","#","#","#"],
+    ["#"," ","#"," ","#"," ","#"," #"," ","#","#","#"],
     ["#"," ","#"," ","#","#","#"," "," ","#","#","#"],
     ["#"," ","#"," ","#","#","#","#","#","#","#","#"],
     ["#"," ","#"," ","#"," ","#","#","#","#","#","#"],
     ["#"," ","#"," ","#"," "," ","#","#","#","#","#"],
-    ["#"," "," "," "," ","#","#"," "," "," "," ","#"],
+    ["#"," "," "," "," "," "," "," "," "," "," ","#"],
     ["#","#","#","#","#","#","#","#","#","#","X","#"]
 ]
 
+
+def print_maze(maze, stdscr, path=[]):
+    BLUE = curses.color_pair(1)
+    RED = curses.color_pair(2)
+
+    for i, row in enumerate(maze):
+        for j,value in enumerate(row):
+            stdscr.addstr(i, j*2, value, RED)
+
+
 def main(stdscr):
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)  #foreG color & BG colro
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)  #foreG color & BG colro
+
     stdscr.clear()
-    stdscr.addstr(0, 0, "hello Viru") #row & col
+    print_maze(maze, stdscr)
     stdscr.refresh()
     stdscr.getch()
 
